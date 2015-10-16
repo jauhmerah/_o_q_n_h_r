19766e6bf17f3df5c7f325726dd8f1043d0e3c5f
anot1207 <nurulfarhanamohdsalim@gmail.com>
2015-10-16 09:07:38 +0000

    stamp 161015.1610
    huhu

--- application/views/main/main.php
+++ application/views/main/main.php
@@ -53,9 +53,9 @@
                             <ul class="list-inline">
                                 <li><a href="#"><i class="fa fa-facebook"></i></a></li>
                                 <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                                <li><a href="#"><i class="fa fa-vine"></i></a></li>
+                                <!--<li><a href="#"><i class="fa fa-vine"></i></a></li>
                                 <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                                <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                                <li><a href="#"><i class="fa fa-dribbble"></i></a></li>-->
                                 <li><a href="#"><i class="fa fa-instagram"></i></a></li>
                             </ul>
                         </div>
