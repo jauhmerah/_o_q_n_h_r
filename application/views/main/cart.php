9f76f8360aa77bba9d00d376fa8eca817eea5a39
amiezamiera <amizasupian@yahoo.com>
2015-10-23 07:11:12 +0000

    stamp 23102015.1510
    new cart

--- application/views/main/cart.php
+++ application/views/main/cart.php
@@ -205,71 +205,14 @@
 
 	<section id="do_action">
 		<div class="container">
-			<div class="heading">
+			<!--div class="heading">
 				<h3>What would you like to do next?</h3>
 				<p>Choose if you have a discount code or reward points you want to use or would like to estimate your delivery cost.</p>
-			</div>
+			</div>-->
 			<div class="row">
+				
 				<div class="col-sm-6">
-					<div class="chose_area">
-						<ul class="user_option">
-							<li>
-								<input type="checkbox">
-								<label>Use Coupon Code</label>
-							</li>
-							<li>
-								<input type="checkbox">
-								<label>Use Gift Voucher</label>
-							</li>
-							<li>
-								<input type="checkbox">
-								<label>Estimate Shipping & Taxes</label>
-							</li>
-						</ul>
-						<ul class="user_info">
-							<li class="single_field">
-								<label>Country:</label>
-								<select>
-									<option>Malaysia</option>
-									<!--option>Bangladesh</option>
-									<option>UK</option>
-									<option>India</option>
-									<option>Pakistan</option>
-									<option>Ucrane</option>
-									<option>Canada</option>
-									<option>Dubai</option>-->
-								</select>
-								
-							</li>
-							<li class="single_field">
-								<label>Region / State:</label>
-								<select>
-									<option>Select</option>
-										<option>Johor</option>
-										<option>Malacca</option>
-										<option>Kelantan</option>
-										<option>Negeri Sembilan</option>
-										<option>Perak</option>
-										<option>Perlis</option>
-										<option>Kedah</option>
-										<option>Terengganu</option>
-										<option>Pahang</option>
-										<option>Penang</option>
-										<option>Selangor</option>
-								</select>
-							
-							</li>
-							<li class="single_field zip-field">
-								<label>Zip Code:</label>
-								<input type="text">
-							</li>
-						</ul>
-						<a class="btn btn-default update" href="">Get Quotes</a>
-						<a class="btn btn-default check_out" href="">Continue</a>
-					</div>
-				</div>
-				<div class="col-sm-6">
-					<div class="total_area">
+					<div class="total_area" >
 						<ul>
 							<li>Cart Sub Total <span>RM59</span></li>
 							<li>Eco Tax <span>RM2</span></li>
