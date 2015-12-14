739ff923208bab4d88927fda189c93631554da42
anot1207 <nurulfarhanamohdsalim@gmail.com>
2015-12-14 04:41:51 +0000

    stamp 14122015.1241

--- application/views/main/souqshop3.php
+++ application/views/main/souqshop3.php
@@ -427,16 +427,16 @@
 								<div class="single-products">
 									<div class="productinfo text-center">
 										<img src="<?php echo base_url();?>assets/anot/images/souq/coffee maker2.jpg" alt="" />
-										<h2>RM <?php echo $data->item_price?></h2>
-										<h5>(<?php echo $data->dinar_price?>)</h5>
+										<h2><?php echo $data->dinar_price?></h2>
+										<h2>(MYR <?php echo $data->item_price?>)</h2>
 										<p><?php echo $data->item_name?></p>
 										
 									</div>
 									<div class="product-overlay">
 										<div class="overlay-content" style="background:#ffa300">
 
-											<h2>RM <?php echo $data->item_price?></h2>
-											<h5>(<?php echo $data->dinar_price?>)</h5>
+										<h2><?php echo $data->dinar_price?></h2>
+										<h2>(MYR <?php echo $data->item_price?>)</h2>
 											<p><?php echo $data->item_name?></p>
 											<a href="<?php echo site_url('main/page/productdetails/z2?item='.$data->item_id.'&cat='.'cat_electronics'); ?>">View</p> 
 											<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
--- application/views/main/souqshop5.php
+++ application/views/main/souqshop5.php
@@ -427,15 +427,15 @@
 								<div class="single-products">
 									<div class="productinfo text-center">
 										<img src="<?php echo base_url();?>assets/anot/images/souq/modenas.jpg" alt="" />
-										<h2>RM <?php echo $data->item_price?></h2>
-										<h5>(<?php echo $data->dinar_price?>)</h5>
+										<h2><?php echo $data->dinar_price?></h2>
+										<h2>(MYR <?php echo $data->item_price?>)</h2>
 										<p><?php echo $data->item_name?></p>
 									</div>
 									<div class="product-overlay">
 										<div class="overlay-content" style="background:#ffa300">
 
-											<h2>RM <?php echo $data->item_price?></h2>
-											<h5>(<?php echo $data->dinar_price?>)</h5>
+										<h2><?php echo $data->dinar_price?></h2>
+										<h2>(MYR <?php echo $data->item_price?>)</h2>
 											<p><?php echo $data->item_name?></p>
 											<a href="<?php echo site_url('main/page/productdetails/z4?item='.$data->item_id.'&cat='.'cat_automotive'); ?>">View</p> 
 											<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
